506443b0e2076ef43f150121fb1f12d653a13963
hdls <myedumail@mail.ru>
2014-01-11 17:04:09 +0200

    добавил один класс. Надеюсь там все правильно

---
+++ src/database/DataBase.java
@@ -0,0 +1,20 @@
+package database;
+
+import java.util.*
+import javax.sql.*;
+
+public abstract class AbstractDataBase {
+
+	protected Connection connect(String driver, String url, String username, String password) throws SQLException {
+		Locale.setDefault(Locale.ENGLISH);
+		Class.forName(driver);
+		return DriverManager.getConnection(url, username, password);
+	}
+
+	public abstract List<Map<String, <T extends Object>> getAll() throws SQLException;
+	public abstract Map<String, <T extends Object> (int id) throws SQLException;
+	public abstract void remove(int id) throws SQLException;
+	public abstract void add(Object... args) throws SQLException;
+	public abstract void modify(Object... args) throws SQLException;
+	public abstract List<Integer> getAllIds() throws SQLException;
+}
