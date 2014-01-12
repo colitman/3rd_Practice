3ae33f8f52a470c36c46879f840cf5b78c2e281c
colitman <colitman@rambler.ru>
2014-01-12 16:39:33 +0200

    entities modified

--- src/database/entities/Country.java
+++ src/database/entities/Country.java
@@ -4,7 +4,30 @@ import java.util.*;
 
 public class Country implements Entity {
 
-	@Override
+	private List<Region> regions;
+	private long id;
+	private String name;
+	private String language; 
+	private long population;
+	private long timeZone;
 	
+	public Country() {
+	
+	}
+	
+	@Override
+	public void setID(int id) {
+		this.id = id;
+	}
+	
+	@Override
+	public int getID() {
+		return id;
+	}
+	
+	@Override
+	public List<Entity> getAllChildren() {
+		return regions;
+	}
 
 }
--- src/database/entities/Entity.java
+++ src/database/entities/Entity.java
@@ -3,7 +3,9 @@ package database.entities;
 import java.util.*;
 
 public interface Entity {
-
+	
+	public void setID(int id);
+	public int getID();
 	public List<Entity> getAllChildren();
 	
 }
