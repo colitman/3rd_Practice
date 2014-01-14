f4a6e9ef02eade46a3f1adc8947082fcef5b7a3d
colitman <colitman@rambler.ru>
2014-01-14 12:59:21 +0200

    fixing smth strange

--- src/hibernate/util/HibernateUtil.java
+++ src/hibernate/util/HibernateUtil.java
@@ -16,14 +16,8 @@ public class HibernateUtil {
 	}
 
 	private static SessionFactory newSessionFactory() throws Exception {
-<<<<<<< HEAD
 		Configuration conf = new Counfiguration().confige("res/hibernate.cfg.xml");
 		StandartServiceRegistryBuilder builder = new StandartServiceRegistryBuilder().applySettings(conf.getProperties());
 		return conf.buildSessionFactory(builder.build());
-=======
-		Configuration configuration = new Configuration().configure();
-		StandardServiceRegistryBuilder builder = new StandardServiceRegistryBuilder().applySettings(configuration.getProperties());
-		return configuration.buildSessionFactory(builder.build());
->>>>>>> d0a1eaf1ea3545625dc8f4eaed8123647a7d619c
 	}
 }
