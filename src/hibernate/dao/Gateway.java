665211a360d77095e39f9b1aa9da5f259baa30e4
colitman <colitman@rambler.ru>
2014-01-14 12:28:45 +0200

    добавил переименованные файлы #12

---
+++ src/hibernate/dao/Gateway.java
@@ -0,0 +1,13 @@
+package hibernate.dao;
+
+import java.sql.*;
+import java.util.*;
+
+public interface Gateway<T> {
+	
+	public void add(T entity) throws SQLException;
+	public void modify(int id, T entity) throws SQLException;
+	public T get(int id) throws SQLException;
+	public Collection<T> getAll() throws SQLException;
+	public void remove(T entity) throws SQLException;
+}
